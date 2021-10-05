cbe79f863671183d9dc9e1f22d72950497bcd5eb
sachin <slpatil66@gmail.com>
2021-10-05 20:13:12 +0530

    first commit

---
+++ src/main/java/com/sachin/controller/MyApplicationController.java
@@ -0,0 +1,15 @@
+package com.sachin.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/application/api")
+public class MyApplicationController {
+	@GetMapping("/")
+	public String getMessage() {
+		return "!!!! Hello World !!!!";
+	}
+
+}
