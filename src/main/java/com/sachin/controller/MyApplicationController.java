808b7ff7d77b5243fbb43134a399aa43565bdfd4
sachin <slpatil66@gmail.com>
2021-11-01 12:08:43 +0530

    logs added.

--- src/main/java/com/sachin/controller/MyApplicationController.java
+++ src/main/java/com/sachin/controller/MyApplicationController.java
@@ -1,5 +1,7 @@
 package com.sachin.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,16 +9,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/application/api")
 public class MyApplicationController {
+	
+	private Logger logger = LoggerFactory.getLogger(MyApplicationController.class);
+	
 	@GetMapping("/")
 	public String getMessage() {
+		logger.info("Inside getMessage() ");
 		return "!!!! Hello World !!!!";
 	}
 	@GetMapping("/hello")
 	public String sayHello() {
+		logger.info("Inside sayHello() ");
 		return "!!!! Hello World 2 !!!!";
 	}
 	@GetMapping("/message")
 	public String message() {
+		logger.info("Inside message() ");
 		return "!!!! My first Springboot Application Runnning on Azure Platform !!!!";
 	}
 
